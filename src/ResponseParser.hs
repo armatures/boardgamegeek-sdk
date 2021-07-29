109dfaebdf95b462a0cac16318a4f7f387ed87aa
Charlie Bevis <charliebevis@gmail.com>
2021-07-29 15:38:58 -0500

    parse a single node

--- src/ResponseParser.hs
+++ src/ResponseParser.hs
@@ -1,10 +1,30 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-module ResponseParser (parse) where
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module ResponseParser (MarketplaceResponse(..), parseMarketplace) where
 
 import           Import
 import qualified RIO.ByteString.Lazy as BL
+import           RIO.List.Partial    (head)
+import qualified RIO.Text            as T
 import           Text.XML
+import           Text.XML.Cursor
 
-parse :: BL.ByteString -> Either SomeException Document
-parse = parseLBS def
+parseMarketplace :: BL.ByteString -> Either SomeException MarketplaceResponse
+parseMarketplace xml =
+ -- let
+   -- cursor :: Either SomeException Cursor
+   -- cursor = fromDocument <$> parseLBS def xml
+ -- in
+   do
+   cursor <- fromDocument <$> parseLBS def xml
+   let
+     nameNode :: [Cursor] = child >=> -- into "items"
+       child >=> -- into "item"
+       laxElement "name" $ cursor
+     c = attribute "value" =<< nameNode
+   pure $ MarketplaceResponse . T.pack . show . head $ c
+   -- pure $ MarketplaceResponse $ T.pack $ show $ (foldMap content c)
+
+data MarketplaceResponse =
+  MarketplaceResponse {marketplaceName :: Text}
